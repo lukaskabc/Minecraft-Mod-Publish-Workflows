dd3adfbdefa17c785dc732a635742958829ed3e4
Lukáš Kaňka <lukaskabc@gmail.com>
2026-07-15 22:58:07 +0200

    [Feat(mod-publish)] Update mod-publish workflow with new gradle tasks

--- publish/buildSrc/src/main/kotlin/cz/lukaskabc/minecraft/ci/publish/task/TaskNames.kt
+++ publish/buildSrc/src/main/kotlin/cz/lukaskabc/minecraft/ci/publish/task/TaskNames.kt
@@ -2,11 +2,23 @@ package cz.lukaskabc.minecraft.ci.publish.task
 
 enum class TaskNames(val taskName: String) {
     /**
-     * The main Mod Publish Plugin task
+     * Publishes a single artifact
+     */
+    PUBLISH_ARTIFACT("publishArtifact"),
+
+    /**
+     * Sends release announcement to Discord
+     */
+    ANNOUNCE_DISCORD("announceDiscord"),
+
+    /**
+     * Sends nightly build announcement to discord
+     */
+    NIGHTLY_ANNOUNCE_DISCORD("nightlyAnnounceDiscord"),
+
+    /**
+     * Internal Mod Publish Plugin task
      * @see me.modmuss50.mpp.MppPlugin
      */
     PUBLISH_MODS("publishMods"),
-    PUBLISH_ARTIFACT("publishArtifact"),
-    ANNOUNCE_DISCORD("announceDiscord"),
-    NIGHTLY_ANNOUNCE_DISCORD("nightlyAnnounceDiscord");
 }
